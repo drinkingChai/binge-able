540f18bbd8f245680c254d6fff4e7a6b31cb7357
Wasif Zaman <teatocode@gmail.com>
2017-12-24 00:28:18 -0500

    finished desktop movie

---
+++ src/components/presentational/Movie.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import Stats from './Stats'
+import Button from './Button'
+import IMDb from './IMDb'
+import IMDbRating from './IMDbRating'
+import Poster from './Poster'
+
+export default function (props) {
+  const { data } = props
+
+  return (
+    <div className='movie'>
+      <div className='movie-info'>
+        <Poster url={ data.poster } />
+        <div className='movie-text-stats'>
+          <div className='movie-text'>
+            <h3>{ data.title }</h3>
+            <span className='year'>{ data.year }</span>
+            <p>{ data.plot }</p>
+          </div>
+
+          <div className='movie-stats'>
+            <Stats label='HOURS' value={ data.hours } />
+            <Stats label='BINGE-ABILITY' value={ data.bingeability } />
+          </div>
+        </div>
+      </div>
+
+      <div className='movie-links'>
+        <IMDb imdbID={ data.imdbID }/>
+        <IMDbRating rating={ data.imdbRating } />
+        <Button label='SOURCES' />
+      </div>
+    </div>
+  )
+}
